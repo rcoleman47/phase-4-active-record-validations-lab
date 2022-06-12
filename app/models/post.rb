e555d1bcaac2f97453269be1160caf0e077be5c6
ryanadams <rcoleman.adams@gmail.com>
2022-06-12 13:18:02 -0500

    commit tree c56489b5575d7c52256fd1294969d42ec831e570

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: ["Fiction", "Non-Fiction"]
+  
+  validate :title_must_contain 
+
+  private
+
+  def title_must_contain
+    words = [/Won't Believe|Secret|Top \d|Guess/]
+    unless (words).any?{|w| w.match?(title)}
+      errors.add(:title, "must contain clickbait")
+    end
+  end
+  
 end
